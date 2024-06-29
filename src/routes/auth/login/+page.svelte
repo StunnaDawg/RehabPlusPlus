d563e4a50901d622935e69fca37ffcba3ca2a01d
StunnaDawg <jonsonallen9@gmail.com>
2024-06-28 22:52:34 -0300

    foundations

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Card from '$lib/components/ui/card/card.svelte';
+	import CardHeader from '$lib/components/ui/card/card-header.svelte';
+	import CardContent from '$lib/components/ui/card/card-content.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<Card>
+	<CardHeader>Login to the site!</CardHeader>
+	<CardContent>
+		<Button href="/auth/login/github">Login to the site</Button>
+	</CardContent>
+</Card>
