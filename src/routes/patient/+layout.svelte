44f3a4be886bc9a977cf8ae782315bf1430e0c25
ryangmurphy <ryangregorymurphy@outlook.com>
2024-11-18 18:26:45 -0400

    refactored clinic and patient dashboards, put sidebar and navigation into layout folder for better readability, and main content will be page file

--- src/routes/patient/+layout.svelte
+++ src/routes/patient/+layout.svelte
@@ -1,8 +1,161 @@
 <script lang="ts">
-  import "../../app.css";
-  import { ModeWatcher } from "mode-watcher";
+	import '../../app.css';
+
+	import {
+		CircleUser,
+		ChartLine,
+		Package,
+		House,
+		ShoppingCart,
+		Bell,
+		Activity,
+		ArrowUpRight,
+		CreditCard,
+		DollarSign,
+		Menu,
+		Package2,
+		Search,
+		Users,
+		Dumbbell,
+		TrendingUp,
+		CalendarDays,
+		Bandage,
+		Bike,
+
+		Notebook
+
+	} from 'lucide-svelte';
+	import { Badge } from '$lib/components/ui/badge';
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card/index';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index';
+	import { Input } from '$lib/components/ui/input/index';
+	import * as Sheet from '$lib/components/ui/sheet/index';
+
+	import ThemeToggle from '../../components/ThemeToggle.svelte';
+	import { ModeWatcher } from 'mode-watcher';
+
+	type NavItem = {
+		href: string;
+		label: string;
+		icon: any; 
+	};
+
+	const navItems: NavItem[] = [
+		{ href: '/patient', label: 'Overview', icon: House },
+		{ href: '/patient/exercises', label: 'Exercises', icon: Dumbbell },
+		{ href: '/patient/progress', label: 'Progress', icon: TrendingUp },
+		{ href: '/patient/resources', label: 'Resources', icon: Notebook }
+	];
+
+	const mobileNavItems: NavItem[] = [
+		{ href: '/patient', label: 'Overview', icon: House },
+		{ href: '/patient/exercises', label: 'Exercises', icon: Dumbbell },
+		{ href: '/patient/progress', label: 'Progress', icon: TrendingUp },
+		{ href: '/patient/resources', label: 'Resources', icon: Notebook }
+	];
+
 </script>
 
-<slot />
+<div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] border-muted">
+	<div class="hidden border-r border-muted bg-muted/40 md:block">
+		<div class="flex h-full max-h-screen flex-col gap-2">
+			<div class="flex h-14 items-center border-b border-muted px-4 lg:h-[60px] lg:px-6">
+				<a href="/" class="flex items-center gap-2 font-semibold">
+					<img src="logo.png" width={30} height={25} alt="RehabPlus Logo" />
+					<span class="">Rehab Plus</span>
+				</a>
+
+				<!-- Notification button for future 
+    <Button variant="outline" size="icon" class="ml-auto h-8 w-8">
+      <Bell class="h-4 w-4" />
+      <span class="sr-only">Toggle notifications</span>
+    </Button>
+    -->
+			</div>
+			<div class="flex-1">
+				<nav class="grid items-start px-2 text-sm font-medium lg:px-4">
+					<p class="flex items-center gap-3 rounded-lg px-3 py-2 text-sm text-muted-foreground/70">
+						Main Menu
+					</p>
+					{#each navItems as { href, label, icon }}
+						<a
+							{href}
+							class="flex items-center gap-3 rounded-lg px-3 py-2 text-lg text-muted-foreground transition-all hover:bg-blue-100 hover:text-primary"
+						>
+							<svelte:component this={icon} class="h-5 w-5 text-primary" />
+							{label}
+						</a>
+					{/each}
+				</nav>
+			</div>
+			<div class="mt-auto p-4"></div>
+		</div>
+	</div>
+
+
+	<!--Mobile Menu-->
+	<div class="flex flex-col">
+		<header
+			class="flex h-14 w-full items-center gap-4 border-muted border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6"
+		>
+			<Sheet.Root>
+				<Sheet.Trigger asChild let:builder>
+					<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
+						<Menu class="h-5 w-5" />
+						<span class="sr-only">Toggle navigation menu</span>
+					</Button>
+				</Sheet.Trigger>
+				<Sheet.Content side="left" class="flex flex-col">
+					<nav class="grid gap-2 text-lg font-medium">
+						<a href="##" class="flex items-center gap-2 text-lg font-semibold">
+							<img src="logo.png" width={30} height={25} alt="RehabPlus Logo" />
+							<span class="">Rehab Plus</span>
+						</a>
+						{#each mobileNavItems as { href, label, icon }}
+							<a
+								{href}
+								class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+							>
+								<svelte:component this={icon} class="h-5 w-5 text-primary" />
+								{label}
+							</a>
+						{/each}
+					</nav>
+					<div class="mt-auto">
+						<Card.Root></Card.Root>
+					</div>
+				</Sheet.Content>
+			</Sheet.Root>
+			<div class="w-full flex-1">
+				<form>
+					<div class="relative"></div>
+				</form>
+			</div>
+			
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger asChild let:builder>
+					<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
+						<CircleUser class="h-8 w-8" />
+						<span class="sr-only">Toggle user menu</span>
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content align="end">
+					<DropdownMenu.Label>My Account</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item>Settings</DropdownMenu.Item>
+					<DropdownMenu.Item>Support</DropdownMenu.Item>
+					<DropdownMenu.Item><ThemeToggle /></DropdownMenu.Item>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item>Logout</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</header>
+
+		<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+			<slot />
+		</main>
+	</div>
+</div>
 
 <ModeWatcher />
---
+++ src/routes/physician/patients/+page.svelte
@@ -0,0 +1,100 @@
+<script>
+
+    import { Badge } from '$lib/components/ui/badge';
+    import { Button } from '$lib/components/ui/button';
+    import * as Card from "$lib/components/ui/card/index";
+    import * as Table from "$lib/components/ui/table/index";
+   
+    
+    // Sample data
+    const exerciseStats = {
+      completed: 24,
+      total: 30,
+      streak: 5
+    };
+
+    const patients = [
+    {
+      id: 1,
+      fullName: "Liam Johnson",
+      email: "liam.johnson@example.com",
+      phone: "(902)123-4546",
+      date: "2024-10-01",
+      diagnosis: "ACL Tear",
+      progress: "50%",
+      status: "active | nonactive"
+    },
+    {
+      id: 2,
+      fullName: "Olivia Smith",
+      email: "olivia.smith@example.com",
+      phone: "(902)123-4546",
+      date: "2024-10-05",
+      diagnosis: "Shoulder Dislocation",
+      progress: "80%",
+    },
+    {
+      id: 3,
+      fullName: "Ethan Brown",
+      email: "ethan.brown@example.com",
+      phone: "(902)123-4546",
+      date: "2024-10-10",
+      diagnosis: "Ankle Sprain",
+      progress: "30%",
+    },
+    {
+      id: 4,
+      fullName: "Emma Davis",
+      email: "emma.davis@example.com",
+      phone: "(902)123-4546",
+      date: "2024-10-15",
+      diagnosis: "Knee Surgery",
+      progress: "95%",
+    },
+  ];
+  
+</script>
+
+<Card.Root>
+    <Card.Header class="px-6">
+      <Card.Title>Patients</Card.Title>
+      <Card.Description>Patients from ProActive Clinic</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.Head class="md:table-cell">Full Name</Table.Head>
+            <Table.Head class="md:table-cell text-left">Date</Table.Head>
+            <Table.Head class="md:table-cell text-left">Diagnosis</Table.Head>
+            <Table.Head class="md:table-cell text-left">Progress</Table.Head>
+            <Table.Head class="md:table-cell text-left">Actions</Table.Head>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {#each patients as patient (patient.id)}
+            <Table.Row>
+              <Table.Cell>
+                <div class="font-medium">{patient.fullName}</div>
+                <div class="text-muted-foreground text-sm md:inline">
+                  {patient.email}
+                </div>
+                <div class="text-muted-foreground text-sm md:inline">
+                  {patient.phone}
+                </div>
+              </Table.Cell>
+              <Table.Cell class="md:table-cell">{patient.date}</Table.Cell>
+              <Table.Cell class="md:table-cell">{patient.diagnosis}</Table.Cell>
+              <Table.Cell class="md:table-cell">
+                {patient.progress}
+              </Table.Cell>
+              <Table.Cell class="md:table-cell">
+                <Button variant="ghost">View</Button>
+                <Button variant="ghost">Edit</Button>
+              </Table.Cell>
+            </Table.Row>
+          {/each}
+        </Table.Body>
+      </Table.Root>
+    </Card.Content>
+  </Card.Root>
